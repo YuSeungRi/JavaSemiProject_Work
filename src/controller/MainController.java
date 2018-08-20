5a6ba5291c0c37563f3284d71769c93ed70055b7
YuSeungri <YuSeungri@192.168.43.69>
2018-08-20 20:53:49 +0900

    ..

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Mainview.jsp")
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
