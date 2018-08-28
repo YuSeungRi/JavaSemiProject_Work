41a8ac1f5be7325fc46d44a7c01420c93708034b
olaf <olaf@DESKTOP-9CNIRNE>
2018-08-28 17:38:39 +0900

    userinfo sql

---
+++ outcomes/userinfo.sql
@@ -0,0 +1,15 @@
+SELECT * FROM userinfo;
+
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user1',' nick1 ',' 1234 ',' 1 ','내용1' );
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user2',' nick2 ',' 1234 ',' 1 ','내용2' );
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user3',' nick3 ',' 1234 ',' 1 ','내용3' );
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user4',' nick4 ',' 1234 ',' 1 ','내용4' );
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user5',' nick5 ',' 1234 ',' 1 ','내용5' );
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user6',' nick6 ',' 1234 ',' 1 ','내용6' );
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user7',' nick7 ',' 1234 ',' 1 ','내용7' );
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user8',' nick8 ',' 1234 ',' 1 ','내용8' );
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user9',' nick9 ',' 1234 ',' 1 ','내용9' );
+INSERT INTO userinfo (user_email, user_nick, user_pw, user_level, user_intro) VALUES ('user10',' nick10 ',' 1234 ',' 1 ','내용10' );
+
+DELETE userinfo;
+
