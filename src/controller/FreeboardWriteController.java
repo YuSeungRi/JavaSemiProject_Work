5cadf32306ffa405a9d71aabd8b286dee9eb1897
Ji <Ji@DESKTOP-029KC44>
2018-09-03 21:16:05 +0900

    ??

--- src/controller/FreeboardWriteController.java
+++ src/controller/FreeboardWriteController.java
@@ -148,22 +148,6 @@ public class FreeboardWriteController extends HttpServlet {
 		
 		service.createBoard(dto);
 		
-//		request.setCharacterEncoding("UTF-8");
-//		
-//		BoardDto dto = new BoardDto();
-//		
-//		dto.setBoardCategory("FreeBoard");
-//		dto.setBoardTitle(request.getParameter("title"));
-//		
-//		dto.setBoardUser((String) request.getSession().getAttribute("userId")); // UserInfoDao 메소드 구현할 때까지 임시로 쓰기
-//		dto.setBoardUser((String) request.getSession().getAttribute("userNick")); // 아직 UserInfoDao 에 메소드 구현 x
-		
-//		dto.setBoardContent(request.getParameter("summernote"));
-		
-//		boolean success = service.createBoard(dto);
-//		System.out.println("FreeboardWriteController_" + success);
-//		System.out.println("FreeboardWriteController_" + dto);
-		
 		// 리다이렉트
 		response.sendRedirect("/Freeboard/free.do");
 	}
