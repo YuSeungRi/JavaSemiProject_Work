ec7db1839362a3a41da4cf6af6a702dc3f48bf37
han92 <han92@DESKTOP-8KRCQOC>
2018-09-06 22:06:37 +0900

    수정

--- src/mail/SMTPAuthenticator.java
+++ src/mail/SMTPAuthenticator.java
@@ -6,7 +6,7 @@ import javax.mail.PasswordAuthentication;
 public class SMTPAuthenticator extends Authenticator{
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("popiui0051@gmail.com","dnsdka30919");
+		return new PasswordAuthentication("popiui0051@gmail.com","qwe123RTY");
 
 	}
 
